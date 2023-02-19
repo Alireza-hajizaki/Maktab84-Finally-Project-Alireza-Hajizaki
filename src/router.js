df4e774509be43e248c131491447ec217917137e
Shahram-hajizaki <shahramhjz75@gmail.com>
2023-02-19 14:10:26 +0330

    create-login-register-pages

--- src/router.js
+++ src/router.js
@@ -3,6 +3,8 @@ import Category from "./Pages/Category/Category";
 import ArticleInfo from "./Pages/ArticleInfo/ArticleInfo";
 import CourseInfo from "./Pages/CourseInfo/CourseInfo";
 import Courses from "./Pages/Courses/Courses";
+import Register from "./Pages/Register/Register";
+import Login from "./Pages/Login/Login";
 
 
 const routes =[
@@ -11,6 +13,8 @@ const routes =[
   { path: "/article-info/:articleName" , element:  <ArticleInfo/> },
   { path: "/course-info/:courseName" , element: <CourseInfo/> },
   { path: "/courses" , element: <Courses/> },
+  { path: "/login" , element: <Login/> },
+  { path: "/register" , element: <Register/> },
 ]
 
 export default routes;

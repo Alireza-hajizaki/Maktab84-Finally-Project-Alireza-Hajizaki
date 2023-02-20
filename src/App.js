0f376ee3335baa5d3b07fe3e4b7cddc64b98d39d
Shahram-hajizaki <shahramhjz75@gmail.com>
2023-02-20 12:37:16 +0330

    create-redux

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import './App.css';
 import {useRoutes} from "react-router-dom";
 import routes from './router';
+import { useSelector} from 'react-redux'
 
 function App() {
 
   const router = useRoutes(routes)
+
+  const courses = useSelector((store) => store.courseInfo)
+  console.log(courses)
 
   return (
     <div className="App">

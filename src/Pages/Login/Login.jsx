f939947e366d4064e186b58f0c92ce6b58c008be
Shahram-hajizaki <shahramhjz75@gmail.com>
2023-02-20 23:03:13 +0330

    create-input-component

--- src/Pages/Login/Login.jsx
+++ src/Pages/Login/Login.jsx
@@ -3,70 +3,74 @@ import "./Login.css";
 import Header from "../../Layout/Header/Header";
 import Footer from "../../Layout/Footer/Footer";
 import {Link} from 'react-router-dom';
+import Input from "../../Compnents/Form/Input"
 
 const Login = () => {
   return (
     <div>
         <Header/>
 
-        <section className="login-register">
-        <div className="login">
-          <span className="login__title">ورود به حساب کاربری</span>
-          <span className="login__subtitle">
+        <section class="login-register">
+        <div class="login">
+          <span class="login__title">ورود به حساب کاربری</span>
+          <span class="login__subtitle">
             خوشحالیم دوباره میبینیمت دوست عزیز :)
           </span>
-          <div className="login__new-member">
-            <span className="login__new-member-text">کاربر جدید هستید؟</span>
-            <Link className="login__new-member-link" to="/register">
+          <div class="login__new-member">
+            <span class="login__new-member-text">کاربر جدید هستید؟</span>
+            <Link class="login__new-member-link" to="/register">
               ثبت نام
             </Link>
           </div>
-          <form action="#" className="login-form">
-            <div className="login-form__username">
-              <input
+          <form action="#" class="login-form">
+            <div class="login-form__username">
+              <Input
                 className="login-form__username-input"
                 type="text"
                 placeholder="نام کاربری یا آدرس ایمیل"
+                element="input"
               />
-              <i className="login-form__username-icon fa fa-user"></i>
+              <i class="login-form__username-icon fa fa-user"></i>
             </div>
-            <div className="login-form__password">
-              <input
+            <div class="login-form__password">
+              <Input
+                element="input"
+                type="password"
                 className="login-form__password-input"
-                type="text"
                 placeholder="رمز عبور"
               />
-              <i className="login-form__password-icon fa fa-lock-open"></i>
+
+              <i class="login-form__password-icon fa fa-lock-open"></i>
             </div>
-            <button className="login-form__btn" type="submit">
-              <i className="login-form__btn-icon fas fa-sign-out-alt"></i>
-              <span className="login-form__btn-text">ورود</span>
+            <button class="login-form__btn" type="submit">
+              <i class="login-form__btn-icon fas fa-sign-out-alt"></i>
+              <span class="login-form__btn-text">ورود</span>
             </button>
-            <div className="login-form__password-setting">
-              <label className="login-form__password-remember">
-                <input className="login-form__password-checkbox" type="checkbox" />
-                <span className="login-form__password-text">
+            <div class="login-form__password-setting">
+              <label class="login-form__password-remember">
+                <input class="login-form__password-checkbox" type="checkbox" />
+                <span class="login-form__password-text">
                   مرا به خاطر داشته باش
                 </span>
               </label>
-              <label className="login-form__password-forget">
-                <a className="login-form__password-forget-link" href="#">
+              <label class="login-form__password-forget">
+                <a class="login-form__password-forget-link" href="#">
                   رمز عبور را فراموش کرده اید؟
                 </a>
               </label>
             </div>
           </form>
-          <div className="login__des">
-            <span className="login__des-title">سلام کاربر محترم:</span>
-            <ul className="login__des-list">
-              <li className="login__des-item">
+          <div class="login__des">
+            <span class="login__des-title">سلام کاربر محترم:</span>
+            <ul class="login__des-list">
+              <li class="login__des-item">
                 لطفا از مرورگر های مطمئن و بروز مانند گوگل کروم و فایرفاکس
                 استفاده کنید.
               </li>
-              <li className="login__des-item">
+              <li class="login__des-item">
                 ما هرگز اطلاعات محرمانه شمارا از طریق ایمیل درخواست نمیکنیم.
               </li>
-              <li className="login__des-item">
+              <li class="login__des-item">
                 لطفا کلمه عبور خود را در فواصل زمانی کوتاه تغییر دهید.
               </li>
             </ul>

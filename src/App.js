4eac5cdbaa6c2479bc81560675c701483439ca2d
Shahram-hajizaki <shahramhjz75@gmail.com>
2023-02-21 11:59:39 +0330

    create-auth-context

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
+import React, {useState} from 'react';
 import './App.css';
 import {useRoutes} from "react-router-dom";
 import routes from './router';
 import { useSelector} from 'react-redux'
+import AuthContext from './Context/authContext';
 
 function App() {
+  const [isLoggedIn , setIsLoggedIn] = useState(false)
+  const [token , setToken] = useState(false)
+  const [userInfos , setUserInfos] = useState({})
+
+  const login =(token) => {
+    setToken(token)
+    localStorage.setItem('user' , JSON.stringify({token}))
+  }
+  const logout = () =>{
+    setToken(null)
+    setUserInfos({})
+    localStorage.removeItem('user')
+  }
 
   const router = useRoutes(routes)
 
@@ -11,8 +26,16 @@ function App() {
   console.log(courses)
 
   return (
-    <div className="App">
+    <div className='App'>
+    <AuthContext.Provider value={{
+       isLoggedIn : isLoggedIn,
+       token: token,
+       userInfos: userInfos,
+       login,
+       logout,
+    }}>
       {router}
+    </AuthContext.Provider>
     </div>
   );
 }

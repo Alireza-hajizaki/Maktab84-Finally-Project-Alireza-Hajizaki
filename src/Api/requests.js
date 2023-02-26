51aaffd96eed178e7611ca50e4bee03294baac58
Shahram-hajizaki <shahramhjz75@gmail.com>
2023-02-26 11:33:19 +0330

    fix

--- src/Api/requests.js
+++ src/Api/requests.js
@@ -1,21 +1,9 @@
-import axios from "axios";
+import React from 'react'
 
-// export const requests = {
-//   getall: function () {
-//     axios
-//       .get("https://63a3f3dc9704d18da099a375.mockapi.io/test")
-//       .then((res) => {
-//         console.log(res);
-//       });
-//   },
-//   // delete: function (setAllUsers, deleteID) {
-//   //   axios
-//   //     .delete(`https://63a3f3dc9704d18da099a375.mockapi.io/test/${deleteID}`)
-//   //     .then((res) => this.getall(setAllUsers));
-//   // },
-//   // edit: function (setAllUsers, editID, data) {
-//   //   axios
-//   //     .put(`https://63a3f3dc9704d18da099a375.mockapi.io/test/${editID}`, data)
-//   //     .then((res) => this.getall(setAllUsers));
-//   // },
-// };
+const requests = () => {
+  return (
+    <div>requests</div>
+  )
+}
+
+export default requests
